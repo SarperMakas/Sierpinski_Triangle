eb87f8b2ba99386941991b5c94fe444598ab341b
SarperMakas <89203120+SarperMakas@users.noreply.github.com>
2023-02-17 09:34:55 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -19,15 +19,18 @@ class Main:
         self.positions = []
 
         self.clock = pygame.time.Clock()
-        self.fps: int = 100
+        self.fps: int = 10
 
-        self.num_edges = 3
-        self.distance = 350
-        self.color = (255, 255, 255)
+        self.num_edges = 6
+        self.n = 1/2
         self.radius = 1
+        self.iter_num = 1
+        self.distance = 300
+
+        self.color = (255, 255, 255)
         self.current_pos = (0, 0)
         self._add_circle = self.add_circle()
-        self.iter_num = 1
+        
     
     def event(self):
         """Main event loop"""
@@ -45,8 +48,11 @@ class Main:
     
     def main(self):
         """Main game loop"""
-        self.draw_circles(self.width//2, self.height//2+50)
-        self.current_pos = self.mid_point(self.positions[0], self.positions[1])
+        self.draw_circles(self.width//2, self.height//2)
+        self.current_pos = self.choose_point(self.positions[0], self.positions[1], self.n)
+
+        pygame.draw.circle(self.screen, self.color, self.rand_point_from_edges(), self.radius)
+
         while self.run:
             self.event()
             self.draw()
@@ -61,7 +67,7 @@ class Main:
         
         while max_degre < 360:
             max_degre += degre
-            yield (max_degre+30) % 360
+            yield max_degre-90
     
     def draw_circles(self, x1, y1):
         """draw circles of the triangle"""
@@ -70,16 +76,29 @@ class Main:
             self.positions.append((x2, y2))
             pygame.draw.circle(self.screen, self.color, (x2, y2), self.radius)
     
-    def mid_point(self, p1, p2):
+    def choose_point(self, p1, p2, n):
         """returns mid point of the two positions"""
-        return (p1[0]+p2[0])/2, (p1[1]+p2[1])/2
+        return abs(p1[0]-p2[0])*n, abs(p1[1]-p2[1])*n
+    
+    def rand_point_from_edges(self):
+        """Choose random position from edges"""
+        points = []
+        positions = self.positions[1:]
+        positions.append(self.positions[0])
+
+        for i, pos in enumerate(positions):
+            point = self.choose_point(pos, self.positions[i], random.random())
+            points.append(point)    
+            pygame.draw.circle(self.screen, self.color, point, self.radius)
+
+        return random.choice(points)
 
     def add_circle(self):
         """Add circle to the screen"""
 
         while True:
             pos = random.choice(self.positions)
-            mid = self.mid_point(self.current_pos, pos)
+            mid = self.choose_point(self.current_pos, pos, self.n)
             self.current_pos = mid
             pygame.draw.circle(self.screen, self.color, (mid[0], mid[1]), self.radius)
             yield
